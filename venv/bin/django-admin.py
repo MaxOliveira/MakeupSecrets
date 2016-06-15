8c23790159ff61b04253bfb33b526a33cc14798d
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-06-15 20:32:46 -0300

    Fixing some errors

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/maxwell/Documentos/UNB/DAS/MakeupSecrets/makeupsecrets/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
